b8286d117eaedd8d06d8aa3b2de4baac49315afd
Suyash Dabhane <spdabhane_m19@ce.vjti.ac.in>
2024-03-04 22:32:12 +0530

    Time: 2 ms (75.07%) | Memory: 45.8 MB (12.46%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -27,20 +27,41 @@ class Solution {
         // return calc(grid.length - 1, grid[0].length - 1, dp , grid);
 
         // Tabulation
-        int[][] dp = new int[grid.length][grid[0].length];
-        dp[0][0] = grid[0][0];
-        for(int col = 1 ; col < grid[0].length ; col++) {
-            dp[0][col] = grid[0][col] + dp[0][col-1];
-        }
-        for(int row = 1; row < grid.length; row++) {
-            dp[row][0] = grid[row][0] + dp[row - 1][0];
+        // int[][] dp = new int[grid.length][grid[0].length];
+        // dp[0][0] = grid[0][0];
+        // for(int col = 1 ; col < grid[0].length ; col++) {
+        //     dp[0][col] = grid[0][col] + dp[0][col-1];
+        // }
+        // for(int row = 1; row < grid.length; row++) {
+        //     dp[row][0] = grid[row][0] + dp[row - 1][0];
+        // }
+
+        // for(int i = 1 ; i < grid.length ; i++) {
+        //     for(int j = 1; j < grid[0].length ; j++) {
+        //         dp[i][j] = grid[i][j] + Math.min( dp[i-1][j] , dp[i][j-1] );
+        //     }
+        // }
+        // return dp[grid.length-1][grid[0].length-1];
+
+
+        // Tabulation with space optimization
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int[] prev = new int[cols];
+        prev[0] = grid[0][0];
+        for(int i = 1 ; i < cols; i++) {
+            prev[i] = prev[i-1] + grid[0][i];
         }
 
-        for(int i = 1 ; i < grid.length ; i++) {
-            for(int j = 1; j < grid[0].length ; j++) {
-                dp[i][j] = grid[i][j] + Math.min( dp[i-1][j] , dp[i][j-1] );
+        for(int row = 1 ; row < rows ; row++) {
+            int[] curr = new int[cols];
+            curr[0] = prev[0] + grid[row][0];
+
+            for(int col = 1; col < cols ; col++) {
+                curr[col] = grid[row][col] + Math.min(curr[col - 1] , prev[col]);
             }
+            prev = curr;
         }
-        return dp[grid.length-1][grid[0].length-1];
+        return prev[cols - 1];
     }
 }
