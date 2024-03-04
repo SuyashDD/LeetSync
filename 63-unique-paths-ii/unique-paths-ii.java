b75a905d5397c7e4c76f6289743c62aa52af0b12
Suyash Dabhane <spdabhane_m19@ce.vjti.ac.in>
2024-03-04 21:18:21 +0530

    Time: 40 ms (5.23%) | Memory: 43.9 MB (6.79%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -25,10 +25,50 @@ class Solution {
         int cols = obstacleGrid[0].length;
         if(obstacleGrid[0][0] == 1) return 0;
 
-        int[][] dp = new int[rows][cols];
-        for(int[] a : dp) Arrays.fill(a , -1);
+        // int[][] dp = new int[rows][cols];
+        // for(int[] a : dp) Arrays.fill(a , -1);
 
-        return calc(rows - 1, cols - 1, obstacleGrid , dp);
-        
+        // return calc(rows - 1, cols - 1, obstacleGrid , dp);
+        int[] prev = new int[cols];
+        prev[0] = 1;
+        for(int i = 1 ; i < cols; i++) {
+            if(obstacleGrid[0][i] != 1 && prev[i - 1] != 0) {
+                prev[i] = 1;
+            }
+            else{
+                prev[i] = 0;
+            }
+        }
+        // System.out.print(" Prev -> ");
+        // for(int i : prev) System.out.print(" " + i + " ");
+
+        for(int row = 1; row < rows; row++){
+            int[] curr = new int[cols];
+            if(prev[0] == 1 && obstacleGrid[row][0] == 0) {
+                curr[0] = 1;
+            }
+            else{
+                curr[0] = 0;
+            }
+
+            for(int col = 1 ; col < cols; col++){
+                if(obstacleGrid[row][col] == 1){
+                    curr[col] = 0;
+                }
+                else{
+                    curr[col] = curr[col - 1] + prev[col];
+                }
+            }
+            
+            System.out.print(" Prev -> ");
+            for(int i : prev) System.out.print(" " + i + " ");
+            System.out.print(" Curr -> ");
+            for(int i : curr) System.out.print(" " + i + " ");
+
+            System.out.print("\n");
+            prev = curr;
+        }
+        return prev[prev.length - 1];
+
     }
 }
