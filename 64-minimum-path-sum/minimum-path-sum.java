f487a7d187ee18b9b528aafbf37966b41ab2412b
Suyash Dabhane <spdabhane_m19@ce.vjti.ac.in>
2024-03-04 22:26:23 +0530

    Time: 3 ms (68.72%) | Memory: 45.5 MB (52.25%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -19,11 +19,28 @@ class Solution {
     }
 
     public int minPathSum(int[][] grid) {
+        // Memoization
+        // int[][] dp = new int[grid.length][grid[0].length];
+        // for(int[] a  : dp) {
+        //     Arrays.fill(a, -1);
+        // }
+        // return calc(grid.length - 1, grid[0].length - 1, dp , grid);
+
+        // Tabulation
         int[][] dp = new int[grid.length][grid[0].length];
-        for(int[] a  : dp) {
-            Arrays.fill(a, -1);
+        dp[0][0] = grid[0][0];
+        for(int col = 1 ; col < grid[0].length ; col++) {
+            dp[0][col] = grid[0][col] + dp[0][col-1];
         }
-        return calc(grid.length - 1, grid[0].length - 1, dp , grid);
-        
+        for(int row = 1; row < grid.length; row++) {
+            dp[row][0] = grid[row][0] + dp[row - 1][0];
+        }
+
+        for(int i = 1 ; i < grid.length ; i++) {
+            for(int j = 1; j < grid[0].length ; j++) {
+                dp[i][j] = grid[i][j] + Math.min( dp[i-1][j] , dp[i][j-1] );
+            }
+        }
+        return dp[grid.length-1][grid[0].length-1];
     }
 }
