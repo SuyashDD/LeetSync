496c324f234926b047a2b6cd20ce6960809255ad
Suyash Dabhane <spdabhane_m19@ce.vjti.ac.in>
2024-03-04 22:33:41 +0530

    Time: 2 ms (75.07%) | Memory: 45.6 MB (29.00%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -20,29 +20,32 @@ class Solution {
 
     public int minPathSum(int[][] grid) {
         // Memoization
-        // int[][] dp = new int[grid.length][grid[0].length];
-        // for(int[] a  : dp) {
-        //     Arrays.fill(a, -1);
-        // }
-        // return calc(grid.length - 1, grid[0].length - 1, dp , grid);
+        /*
 
-        // Tabulation
-        // int[][] dp = new int[grid.length][grid[0].length];
-        // dp[0][0] = grid[0][0];
-        // for(int col = 1 ; col < grid[0].length ; col++) {
-        //     dp[0][col] = grid[0][col] + dp[0][col-1];
-        // }
-        // for(int row = 1; row < grid.length; row++) {
-        //     dp[row][0] = grid[row][0] + dp[row - 1][0];
-        // }
+        int[][] dp = new int[grid.length][grid[0].length];
+        for(int[] a  : dp) {
+            Arrays.fill(a, -1);
+        }
+        return calc(grid.length - 1, grid[0].length - 1, dp , grid);
 
-        // for(int i = 1 ; i < grid.length ; i++) {
-        //     for(int j = 1; j < grid[0].length ; j++) {
-        //         dp[i][j] = grid[i][j] + Math.min( dp[i-1][j] , dp[i][j-1] );
-        //     }
-        // }
-        // return dp[grid.length-1][grid[0].length-1];
+        Tabulation
+        int[][] dp = new int[grid.length][grid[0].length];
+        dp[0][0] = grid[0][0];
+        for(int col = 1 ; col < grid[0].length ; col++) {
+            dp[0][col] = grid[0][col] + dp[0][col-1];
+        }
+        for(int row = 1; row < grid.length; row++) {
+            dp[row][0] = grid[row][0] + dp[row - 1][0];
+        }
 
+        for(int i = 1 ; i < grid.length ; i++) {
+            for(int j = 1; j < grid[0].length ; j++) {
+                dp[i][j] = grid[i][j] + Math.min( dp[i-1][j] , dp[i][j-1] );
+            }
+        }
+        return dp[grid.length-1][grid[0].length-1];
+        
+        */
 
         // Tabulation with space optimization
         int rows = grid.length;
