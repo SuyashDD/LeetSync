55b5ecf5e036ae09c35117af7a5ddec94ad91083
Suyash Dabhane <spdabhane_m19@ce.vjti.ac.in>
2024-06-18 10:26:10 +0530

    Time: 4 ms (71.58%) | Memory: 42 MB (72.55%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -1,40 +1,65 @@
+// class Solution {
+//     public String removeOuterParentheses(String s) {
+//         boolean op = false;
+//         int open = 0;
+//         int close = 0;
+//         StringBuilder str = new StringBuilder();
+//         StringBuilder tmp = new StringBuilder();
+
+
+//         for(int i = 0 ; i < s.length() ; i++){
+//             char cch = s.charAt(i);
+
+//             if(cch == '('){
+//                 if(op == true){
+//                     open += 1;
+//                     tmp.append("(");
+//                 }
+//                 else{
+//                     op = true;
+//                 }
+//             }
+//             else{
+//                 if(op == true && open > 0){
+//                     close += 1;
+//                     tmp.append(")");
+//                     if(open > 0 && close > 0 && open == close){
+//                         str.append(tmp);
+//                         tmp = new StringBuilder();
+//                         open = 0;
+//                         close = 0;
+//                     }
+//                 }
+//                 else{
+//                     op = false;
+//                 }
+//             }
+//         }
+//         return str.toString();
+//     }
+// }
+
 class Solution {
     public String removeOuterParentheses(String s) {
-        boolean op = false;
-        int open = 0;
-        int close = 0;
-        StringBuilder str = new StringBuilder();
-        StringBuilder tmp = new StringBuilder();
+        StringBuilder result = new StringBuilder();
+        int openCount = 0;
 
-
-        for(int i = 0 ; i < s.length() ; i++){
-            char cch = s.charAt(i);
-
-            if(cch == '('){
-                if(op == true){
-                    open += 1;
-                    tmp.append("(");
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if (c == '(') {
+                if (openCount > 0) {
+                    result.append(c);
                 }
-                else{
-                    op = true;
-                }
-            }
-            else{
-                if(op == true && open > 0){
-                    close += 1;
-                    tmp.append(")");
-                    if(open > 0 && close > 0 && open == close){
-                        str.append(tmp);
-                        tmp = new StringBuilder();
-                        open = 0;
-                        close = 0;
-                    }
-                }
-                else{
-                    op = false;
+                openCount++;
+            } else {
+                openCount--;
+                if (openCount > 0) {
+                    result.append(c);
                 }
             }
         }
-        return str.toString();
+        
+        return result.toString();
     }
 }
