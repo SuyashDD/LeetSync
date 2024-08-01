b915df74d79582dab5ec41d36ccbbad51f61d88b
Suyash Dabhane <spdabhane_m19@ce.vjti.ac.in>
2024-08-01 07:25:16 +0530

    Time: 121 ms (10.67%) | Memory: 45.1 MB (5.54%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.java
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.java
@@ -1,39 +1,45 @@
 class Solution {
     public boolean eligible(int i , int j , int[][] vis , int[][] grid){
-        if(i >= 0 && j >= 0 && i < grid.length && j < grid[0].length 
-                && grid[i][j] != 0 && vis[i][j] == 0){
-                    return true;
-                }
-                return false;
+        if(i >= 0 && j >= 0 
+            && i < grid.length
+            && j < grid[0].length
+            && vis[i][j] == 0
+            && grid[i][j] != 0){
+                return true;
+            }
+            return false;
+
     }
+
     public int dfs(int i , int j , int[][] vis , int[][] grid){
         vis[i][j] = 1;
-        int[] dx = {1 , 0 , -1 ,  0};
-        int[] dy = {0 , 1 ,  0 , -1};
-        int ans = 0;
+        int[] dx = {1,0,-1,0};
+        int[] dy = {0,1,0,-1};
+        int curr_ans = 0;
         for(int k = 0 ; k < 4 ; k++){
-            int newx = i + dx[k];
-            int newy = j + dy[k];
-            if(eligible(newx , newy , vis , grid)){
-                ans = Math.max(ans , dfs(newx , newy , vis , grid));
+            int ni = i + dx[k];
+            int nj = j + dy[k];
+
+            if(eligible(ni , nj , vis , grid)){
+                curr_ans = Math.max(curr_ans , dfs(ni , nj , vis , grid));
             }
         }
         vis[i][j] = 0;
-        return ans + grid[i][j];
-
+        return grid[i][j] + curr_ans;
     }
     public int getMaximumGold(int[][] grid) {
-        int[][] vis = new int[grid.length][grid[0].length];
-        for(int[] a : vis){
-            Arrays.fill(a , 0);
-        }
         int n = grid.length;
         int m = grid[0].length;
+        int[][] vis = new int[n][m];
+
+        
+        
         int ans = 0;
         for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
+            for(int j  = 0 ; j < m ; j++){
                 if(grid[i][j] != 0){
-                    ans = Math.max(ans , dfs(i , j , vis , grid));
+                    int tans = dfs(i , j , vis , grid);
+                    ans = Math.max(ans , tans);
                 }
             }
         }
